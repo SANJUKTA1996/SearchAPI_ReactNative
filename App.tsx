b162e002ff190feb3280153f6eaa8c73de364561
SANJUKTA1996 <sanjuktasahoo734@gmail.com>
2023-06-08 17:41:40 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,44 @@
+
+
+import React, { useState } from 'react';
+
+import {
+  Text,
+
+  TextInput,
+
+  View,
+} from 'react-native'; 
+function App(){
+  const [data,setData]=useState([]);
+  const searchUser= async(text)=>{
+   const url=`http://10.0.2.2:3000/users?q=${text}`;
+  // console.warn(url);
+   const response=await fetch(url);
+   const result=await response.json();
+   if(result){
+setData(result);
+   }
+
+  }
+ 
+  return (
+  <View style={{flex:1}}>
+    <Text>Simple API in REact NAtive</Text>
+    <TextInput style={{borderColor:'skyblue',borderWidth:1,margin:15,fontSize:20}}
+    placeholder='Search'
+     onChangeText={(text)=>searchUser(text)}/>
+     {
+      data.length?
+      data.map((item)=><View style={{padding:10,flexDirection:'row',justifyContent:'space-between'}}>
+        <Text style={{fontSize:20}}>{item.name}</Text>
+        <Text style={{fontSize:20}}>{item.age}</Text>
+        <Text style={{fontSize:20}}>{item.email}</Text>
+      </View>
+      ):null
+     }
+  </View>
+  );
+}
+
+export default App;
